e90147d9ed8612c823e6adba6dbc4eeeb04826ae
Tharbouch <112890451+soufianeargane@users.noreply.github.com>
2023-09-20 14:07:45 +0100

    working on horisontal squares. I am almost done with it and the same logic to vertical

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // get html elements
 const gameBoard = document.getElementById('gameBoard');
 
+// set initial variables
+let tracking_game = Array(20).fill().map(() => Array(20).fill(''));
+console.log(tracking_game);
 let currentPlayer = 'X';
 
 // create game board
@@ -19,8 +22,33 @@ function handleClick(event) {
     const row = event.target.dataset.row;
     const col = event.target.dataset.col;
     console.log("position is" +row, col);
+    tracking_game[row][col] = currentPlayer;
     event.target.textContent = currentPlayer;
     event.target.classList.add(currentPlayer);
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    event.target.removeEventListener('click', handleClick);
+    if( checkWin()){
+        console.warn("it is over. " + currentPlayer + "  Wins")
+    }else{
+        //i need a condition hna
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        event.target.removeEventListener('click', handleClick);
+        console.log(tracking_game);
+    }
+}
+
+function checkWin() {
+    console.log("messi");
+    // Check for horizontal wins
+    for (let i = 0; i < 20; i++) {
+        for (let j = 0; j < 16; j++) {
+            if(tracking_game[i][j]=== currentPlayer &&
+                tracking_game [i][j + 1] === currentPlayer &&
+                tracking_game [i][j + 2] === currentPlayer &&
+                tracking_game [i][j + 3] === currentPlayer &&
+                tracking_game [i][j + 4] === currentPlayer
+            ){
+                return true;
+            }
+        }
+    }
+    return false;
 }
